ec295e501f4508f8227133679889f2f409a422ae
PBCDev210 <pbc210.dev@gmail.com>
2026-09-15 05:19:55 +0700

    emacs: setup init

--- emacs/init.el
+++ emacs/init.el
@@ -1,13 +1,44 @@
-(doom! :input ...
-       :file-management ...
-       :completion ...
-       :ui ...
-       :editor ...
-       :emacs ...
-       :term ...
-       :tools ...
-       :os ...
-       :lang ...
-       :email ...
-       :app ...
-       :config ...)
+;;; init.el -*- lexical-binding: t; -*-
+
+(doom! :input
+
+       :completion
+       (corfu +orderless)
+       vertico
+
+       :ui
+       doom
+       !modeline
+       ophints
+       (popup +defaults)
+
+       :editor
+       (evil +everywhere)
+       file-templates
+       fold
+
+       :emacs
+       dired
+       electric
+       undo
+       vc
+
+       :term
+       vterm
+
+       :checkers
+       syntax
+
+       :tools
+       (lsp +eglot)
+       magit
+
+       :os
+       (:if IS-MAC macos)
+
+       :lang
+       emacs-lisp
+       (org +pretty)
+
+       :config
+       (default +bindings +smartparens))
