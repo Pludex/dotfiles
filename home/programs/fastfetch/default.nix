6cde11444cc3c9501e8e2621fb4f79622807fb0e
PBCDev210 <pbc210.dev@gmail.com>
2026-09-19 05:40:51 +0700

    fastfetch: fix

--- home/programs/fastfetch/default.nix
+++ home/programs/fastfetch/default.nix
@@ -4,5 +4,5 @@
     enable = true;
   };
   xdg.configFile."fastfetch/config.jsonc".source = ./config.jsonc;
-  xdg.configFile."fastfetch/logo/nixos.webp".source = "${pkgs.myPkgs.assets}/logo/nixos.webp";
+  xdg.configFile."fastfetch/logo/nixos.webp".source = "${pkgs.myPkgs.assets}/icons/nixos.webp";
 }
